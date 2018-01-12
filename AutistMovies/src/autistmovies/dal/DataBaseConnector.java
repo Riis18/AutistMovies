88a2450e1780c0d7fc1e70ca2214a9cd5353fa5b
Larsh95 <32057928+Larsh95@users.noreply.github.com>
2018-01-12 11:30:40 +0100

    db connector
    added connector

--- AutistMovies/src/autistmovies/dal/DataBaseConnector.java
+++ AutistMovies/src/autistmovies/dal/DataBaseConnector.java
@@ -9,6 +9,17 @@ package autistmovies.dal;
  *
  * @author Jesper
  */
-public class DataBaseConnector {
-    
+
+private SQLServerDataSource dataSource;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+
+public class DataBaseConnector 
+{
+        dataSource = new SQLServerDataSource();
+        dataSource.setServerName("EASV-DB2");
+        dataSource.setPortNumber(1433);
+        dataSource.setDatabaseName("Autistify");
+        dataSource.setUser("CS2017A_15");
+        dataSource.setPassword("Bjoernhart1234");
 }
