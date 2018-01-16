005b8b0f1f96ef12c535e70cf8e4e7aac225381a
Oliver Sylvest Hansen <oliv9858@easv365.dk>
2018-01-16 11:12:23 +0100

    updated ui

--- AutistMovies/src/autistmovies/gui/controller/MainViewController.java
+++ AutistMovies/src/autistmovies/gui/controller/MainViewController.java
@@ -5,9 +5,16 @@
  */
 package autistmovies.gui.controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 
 /**
  * FXML Controller class
@@ -16,12 +23,26 @@ import javafx.fxml.Initializable;
  */
 public class MainViewController implements Initializable {
 
+    @FXML
+    private JFXButton clearBtn;
+    @FXML
+    private JFXButton playPause;
+    @FXML
+    private JFXTextField txtSearch;
+    private MediaView media;
+    @FXML
+    private JFXButton fullscreen;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }    
+
+    @FXML
+    private void play(ActionEvent event) {
+    }
     
 }
